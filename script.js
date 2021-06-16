4a71de1411f56b57a4b8a5d085f3e76df15e0f4d
Oscar Dominguez <dominguez.celada@gmail.com>
2021-06-16 15:39:14 +0200

    feat(script): implement add/delete label for all issues of a repo

--- script.js
+++ script.js
@@ -9,4 +9,32 @@
  * @param {string} options.label Select the label you want to be added (or removed)
  * @param {boolean} [options.remove] Select if you want the label to be removed (instead of added)
  */
-export async function script(octokit, repository, { label, remove }) {}
+export async function script(octokit, repository, { label, remove }) {
+	const owner = repository.owner.login
+	const repo = repository.name
+
+	const issues = await octokit.request('GET /repos/{owner}/{repo}/issues', {
+		repo,
+		owner,
+	});
+	
+	for (const issue of issues.data) {
+		const issueNumber = issue.number
+		
+		if(remove) {
+			await octokit.request('DELETE /repos/{owner}/{repo}/issues/{issue_number}/labels/{name}', {
+				issue_number: issueNumber,
+				name: label,
+				owner,
+				repo,
+			})
+		} else {
+			await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/labels', {
+				issue_number: issueNumber,
+				labels: [label],
+				owner,
+				repo,
+			})
+		}
+	}	
+}
